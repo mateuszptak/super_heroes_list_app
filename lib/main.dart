e3bf50376261cedb23234c7e667cbdf11dd18e5a
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2022-06-13 16:28:00 +0200

    Update minSdkversion from 16 to 21 and targetSdkVersion 31

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Super Heroes!',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
