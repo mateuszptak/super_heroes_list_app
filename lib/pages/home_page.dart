aa0e51a178d01be7ce897e0c75e497e51fb0fa0b
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2022-06-13 20:08:09 +0200

    Implement PageView Builder with all data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,9 +5,14 @@ import 'package:google_fonts/google_fonts.dart';
 import '../models/super_hero_model.dart';
 import '../widgets/add_hero_button.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,27 +34,34 @@ class HomePage extends StatelessWidget {
               ),
             ),
             StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                stream:
-                    FirebaseFirestore.instance.collection('heroes').snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-
-                  final documents = snapshot.data!.docs;
-
-                  return SuperHeroModel(
-                    realName: (documents[0]['real_name']),
-                    nickName: (documents[0]['nick_name']),
-                    avatarImage: (documents[0]['avatar_image']),
-                    quote: (documents[0]['quote']),
+              stream:
+                  FirebaseFirestore.instance.collection('heroes').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                }),
+                }
+                final documents = snapshot.data!.docs;
+                return Expanded(
+                  child: PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapshot.data?.docs.length,
+                    itemBuilder: (context, index) {
+                      return SuperHeroModel(
+                        realName: (documents[index]['real_name']),
+                        nickName: (documents[index]['nick_name']),
+                        avatarImage: (documents[index]['avatar_image']),
+                        quote: (documents[index]['quote']),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
             const AddHeroButton(),
           ],
         ),
